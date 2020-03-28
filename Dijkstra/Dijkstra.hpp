5782fec198c22775a94611bb91469834f09967f2
bvlourenco <bernardovlourenco@tecnico.ulisboa.pt>
2020-03-28 20:30:25 +0000

    Dijkstra: bug fix

--- Dijkstra/Dijkstra.hpp
+++ Dijkstra/Dijkstra.hpp
@@ -4,8 +4,8 @@
 #include "Graph.hpp"
 #include <queue>
 
-void InitializeSingleSource(vector<int> &dvalues, vector<int> &pivalues, int indexVertex, int numVertex);
-void initPriorityQueue(const vector<int> &dvalues, priority_queue <int, vector<pair<int, int>>, greater<int>> &Q, int numVertex);
+void InitializeSingleSource(vector<int> &dvalues, vector<int> &pivalues, int indexVertex);
+void initPriorityQueue(const vector<int> &dvalues, priority_queue <int, vector<pair<int, int>>, greater<pair<int, int>>> &Q, int numVertex);
 void Relax(vector<int> &dvalues, priority_queue <int, vector<pair<int, int>>, greater<pair<int, int>>> &Q, vector<int> &pivalues, pair<int,int> u, pair<int,int> v, int vdvalue);
 void algorithmDijkstra(const Graph &g, int indexVertex);
 
